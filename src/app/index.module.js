c467e22ce0f64c332713899e08ad4cfb78fa7a1d
Levin Calado <levincalado@gmail.com>
2017-08-09 21:44:20 +0800

    yo-angular-gulp-heroku: initial commit

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('yo-angular-gulp-heroku', ['ngAnimate', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngRoute', 'ngMaterial', 'toastr']);
+
+})();
